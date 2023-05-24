a3d6cc76af7e84d16c9102248972f812809ff52d
Ashwini Nagargoje <93382273+Ashwininagargoje703@users.noreply.github.com>
2023-05-24 20:52:13 +0530

    Delete Backend directory

--- Backend/configs/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const connect = () => {
-  mongoose.connect(
-    "mongodb+srv://ashwini:ashwini1234@cluster0.seroub9.mongodb.net/Shopping-store"
-  );
-};
-module.exports = connect;
